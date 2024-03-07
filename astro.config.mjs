f28667211d8f0f42dcd5ff50105eb3bc3c4a8d00
dirtTW <31981880+yichifauzi@users.noreply.github.com>
2024-03-07 14:45:00 +0700

    First Build of astro site with remake of my website

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://yichifauzi.github.io",
+  base: "/DBOP-Installtutorial",
+  integrations: [tailwind(), react(),preact()]
+});
