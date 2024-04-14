9ff5fda3dd89c97abc5e1cd0f242183f26f910c3
dirtTW <31981880+yichifauzi@users.noreply.github.com>
2024-04-14 18:58:05 +0700

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,astro}"],
-  darkMode: "class",
-  theme: {},
-  plugins: [],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
 };
