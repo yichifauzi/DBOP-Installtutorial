41d13dba6200bc726bd86b749860736aef42faf3
dirtTW <31981880+yichifauzi@users.noreply.github.com>
2024-04-14 17:17:32 +0700

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,28 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
+
+const SERVER_PORT = 3000;
+// the url to access your blog during local development
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+// the url to access your blog after deploying it somewhere (Eg. Netlify)
+const LIVE_URL = "";
+// this is the astro command your npm script runs
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+// When you're building your site in local or in CI, you could just set your URL manually
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://yichifauzi.github.io",
   base: "/DBOP-Installtutorial",
-  integrations: [tailwind(), react(), preact()],
+  integrations: [tailwind(), react(), sitemap()],
   prefetch: {
-    prefetchAll: true,
-  },
+    prefetchAll: true
+  }
 });
