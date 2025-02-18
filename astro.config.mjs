52d0502f1450ffe9f15fc363a7f6e01d14676408
dirtTW <a0932636771@gmail.com>
2025-02-18 13:03:19 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
 
 const SERVER_PORT = 3000;
